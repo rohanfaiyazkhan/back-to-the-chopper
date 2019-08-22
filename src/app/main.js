54031e1c663bdb73cff71bfedf4e0dff486faca3
Rohan Faiyaz Khan <rohankhan170@gmail.com>
2019-08-23 01:23:26 +0600

    Added jump mechanics with space

--- src/app/main.js
+++ src/app/main.js
@@ -1,34 +1,50 @@
-import { init, Sprite, GameLoop } from "kontra";
+import { init, Sprite, GameLoop, initKeys, keyPressed } from 'kontra'
 
-let { canvas } = init();
+let { canvas } = init()
+
+let constants = {
+	gravity   : 2,
+	jumpSpeed : 30
+}
 
 let sprite = Sprite({
-  x: 0, // starting x,y position of the sprite
-  y: 360,
-  color: "red", // fill color of the sprite rectangle
-  width: 20, // width and height of the sprite rectangle
-  height: 40,
-  dx: 5 // move the sprite 2px to the right every frame
-});
+	x      : 0,
+	y      : 360,
+	color  : 'red',
+	width  : 20,
+	height : 40,
+	dx     : 7,
+	dy     : 0
+})
+
+initKeys()
 
 let loop = GameLoop({
-  // create the main game loop
-  update: function() {
-    // update the game state
-    sprite.update();
+	// create the main game loop
+	update : function () {
+		// update the game state
+		sprite.update()
 
-    // wrap the sprites position when it reaches
-    // the edge of the screen
-    if (sprite.x > canvas.width) {
-      sprite.dx = -5;
-    } else if (sprite.x < 0) {
-      sprite.dx = 5;
-    }
-  },
-  render: function() {
-    // render the game state
-    sprite.render();
-  }
-});
+		// wrap the sprites position when it reaches
+		// the edge of the screen
+		if (sprite.x + sprite.width + sprite.dx > canvas.width || sprite.x < 0) {
+			sprite.dx *= -1
+		}
 
-loop.start(); // start the game
+		if (sprite.y < 360) {
+			sprite.dy += constants.gravity
+		} else {
+			sprite.dy = 0
+		}
+
+		if (keyPressed('space') && sprite.y === 360) {
+			sprite.dy = -constants.jumpSpeed
+		}
+	},
+	render : function () {
+		// render the game state
+		sprite.render()
+	}
+})
+
+loop.start() // start the game
