983f1a0c6a63c054e61cde77bf112a6b07589732
Abrar Rauf <abrarrauf@Abrars-MacBook-Pro.local>
2019-08-24 17:21:12 +0600

    Added obstacle

--- src/app/main.js
+++ src/app/main.js
@@ -1,50 +1,74 @@
-import { init, Sprite, GameLoop, initKeys, keyPressed } from 'kontra'
+import { init, Sprite, GameLoop, initKeys, keyPressed } from "kontra";
 
-let { canvas } = init()
+let { canvas } = init();
 
 let constants = {
-	gravity   : 2,
-	jumpSpeed : 30
+  gravity: 2,
+  jumpSpeed: 20,
+  height: 40,
+  width: 20
+};
+
+let player = Sprite({
+  x: 0,
+  y: 360,
+  color: "red",
+  width: constants.width,
+  height: constants.height,
+  dx: 5,
+  dy: 0
+});
+
+function isOverlapping(player, obstacle) {
+  return (
+    player.x + player.width + player.dx >= obstacle.x &&
+    player.x <= obstacle.x + obstacle.width &&
+    player.y + player.height + player.dy >= obstacle.y &&
+    player.y + player.height + player.dy <= obstacle.y + obstacle.height
+  );
 }
 
-let sprite = Sprite({
-	x      : 0,
-	y      : 360,
-	color  : 'red',
-	width  : 20,
-	height : 40,
-	dx     : 7,
-	dy     : 0
-})
+let obstacle = Sprite({
+  x: canvas.width / 2,
+  y: 360,
+  height: constants.height,
+  width: 100,
+  color: "green"
+});
 
-initKeys()
+initKeys();
 
 let loop = GameLoop({
-	// create the main game loop
-	update : function () {
-		// update the game state
-		sprite.update()
+  // create the main game loop
+  update: function() {
+    // update the game state
+    player.update();
+    obstacle.update();
+    // wrap the sprites position when it reaches
+    // the edge of the screen
+    if (player.x + player.width + player.dx > canvas.width || player.x < 0) {
+      player.dx *= -1;
+    }
 
-		// wrap the sprites position when it reaches
-		// the edge of the screen
-		if (sprite.x + sprite.width + sprite.dx > canvas.width || sprite.x < 0) {
-			sprite.dx *= -1
-		}
+    if (isOverlapping(player, obstacle)) {
+      player.dx *= -1;
+    }
 
-		if (sprite.y < 360) {
-			sprite.dy += constants.gravity
-		} else {
-			sprite.dy = 0
-		}
+    if (player.y < 360 && !isOverlapping(player, obstacle)) {
+      player.dy += constants.gravity;
+    } else {
+      player.dy = 0;
+    }
 
-		if (keyPressed('space') && sprite.y === 360) {
-			sprite.dy = -constants.jumpSpeed
-		}
-	},
-	render : function () {
-		// render the game state
-		sprite.render()
-	}
-})
+    if (keyPressed("space") && player.y === 360) {
+      player.dy = -constants.jumpSpeed;
+    }
+  },
+  render: function() {
+    // render the game state
+    player.render();
+    obstacle.render();
+  }
+});
 
-loop.start() // start the game
+loop.start(); // start the game
